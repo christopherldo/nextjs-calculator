f1aae1b93ca9ab6268c633158247168cc465bbdb
Christopher de Oliveira <christopherldo@gmail.com>
2024-02-20 15:11:00 -0400

    Adjusts style to make it more responsible

--- components/Result.tsx
+++ components/Result.tsx
@@ -1,6 +1,6 @@
 export default function Result() {
   return (
-    <div className="custombp:text-6xl text-right text-[56px] sm:text-[96px]">
+    <div className="custombp:text-5xl text-right text-[56px] sm:text-[96px]">
       1,258.2
     </div>
   );
